4f3ee5e03c98e0ba0eaf6358a53ee0611184f55a
piyushmishra-bidgely <piyushmishra@bidgely.com>
2024-04-01 19:47:40 +0530

    init spring app

---
+++ src/main/java/com/piyushmishrabidgely/tacocloudapp/TacoCloudAppApplication.java
@@ -0,0 +1,13 @@
+package com.piyushmishrabidgely.tacocloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyushmishrabidgely/tacocloudapp/TacoCloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyushmishrabidgely.tacocloudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
